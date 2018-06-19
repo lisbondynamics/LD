e728f42ddb05f556816f51cff415c697685d76d9
jpcovas <jcovas@gmail.com>
2018-06-19 22:34:52 +0100

    Initial commit

---
+++ js/main-index-slider.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+	$('.intro-slider').owlCarousel({
+		singleItem: true,
+		pagination: true,
+		autoPlay: 7000,
+		itemsDesktop: [1000, 4], //5 items between 1000px and 901px
+        itemsDesktopSmall: [900, 2], // betweem 900px and 601px
+        itemsTablet: [600, 1], //2 items between 600 and 0
+        itemsMobile: false, // itemsMobile disabled - inherit from itemsTablet option
+        autoplay: true
+	});
+ 
+});
